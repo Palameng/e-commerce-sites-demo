baa044b6c12c7ffb922feb64d2dd7db33c21f24b
Palameng <506357902@qq.com>
2018-07-12 16:47:02 +0800

    add some user function

--- src/main/java/com/mymall/controller/portal/UserController.java
+++ src/main/java/com/mymall/controller/portal/UserController.java
@@ -21,20 +21,129 @@ public class UserController {
 
     /**
      * 登录功能
+     *
      * @param username 前端传入的用户名
      * @param password 前端传入的密码
-     * @param session
-     * @return
+     * @param session  session
+     * @return ServerResponse<User>
      */
-    @RequestMapping(value="login.do", method = RequestMethod.POST)
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session){
+    public ServerResponse<User> login(String username, String password, HttpSession session) {
         ServerResponse<User> response = iUserService.login(username, password);
 
-        if (response.isSuccess()){
+        if (response.isSuccess()) {
             session.setAttribute(Const.CURRENT_USER, response.getData());
         }
         return response;
     }
 
+    /**
+     * 登出操作
+     *
+     * @param session session
+     * @return ServerResponse<String>
+     */
+    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session) {
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    /**
+     * 注册
+     *
+     * @param user 用户
+     * @return ServerResponse<String>
+     */
+    @RequestMapping(value = "register.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> register(User user) {
+        return iUserService.register(user);
+    }
+
+
+    /**
+     * 校验账号类型（账号或邮箱）的有效性
+     * @param str 传入的内容
+     * @param type 类型
+     * @return ServerResponse<String>
+     */
+    @RequestMapping(value = "check_valid.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> checkValid(String str, String type) {
+        return iUserService.checkValid(str, type);
+    }
+
+    /**
+     * 获取登录用户信息
+     * @param session session
+     * @return ServerResponse<User>
+     */
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+
+        if (user != null){
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录");
+    }
+
+    /**
+     * 根据用户名搜索重置密码的安全问题
+     * @param username 用户名
+     * @return ServerResponse<String>
+     */
+    @RequestMapping(value = "forget_get_question.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetGetQuestion(String username){
+        return iUserService.selectQuestion(username);
+    }
+
+    /**
+     * 检索用户名-问题-答案 是否匹配， 匹配则通过并添加token加入cache
+     * @param username 用户名
+     * @param question 问题
+     * @param answer 答案
+     * @return ServerResponse<String>
+     */
+    @RequestMapping(value = "forget_check_answer.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetCheckAnswer(String username, String question, String answer){
+        return iUserService.checkAnswer(username, question, answer);
+    }
+
+    /**
+     * 未登录状态下忘记密码
+     * @param usename 用户名
+     * @param passwordNew 新密码
+     * @param forgetToken 前端传递下来的token
+     * @return ServerResponse<String>
+     */
+    @RequestMapping(value = "forget_reset_password.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetResetPassword(String usename, String passwordNew, String forgetToken){
+        return iUserService.forgetResetPassword(usename, passwordNew, forgetToken);
+    }
+
+    /**
+     * 登录状态下修改密码
+     * @param session
+     * @param passwordOld
+     * @param passwordNew
+     * @return
+     */
+    @RequestMapping(value = "reset_password.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> resetPassword(HttpSession session, String passwordOld, String passwordNew){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+
+        return iUserService.resetPassword(passwordOld, passwordNew, user);
+    }
 }
--- src/main/java/com/mymall/service/IUserService.java
+++ src/main/java/com/mymall/service/IUserService.java
@@ -4,5 +4,24 @@ import com.mymall.common.ServerResponse;
 import com.mymall.pojo.User;
 
 public interface IUserService {
+    //登录
     ServerResponse<User> login(String username, String password);
+
+    //注册
+    ServerResponse register(User user);
+
+    //校验账号类型（账号或邮箱）的有效性
+    ServerResponse<String> checkValid(String str, String type);
+
+    //通过用户名查找到相应用户设置的提示问题
+    ServerResponse selectQuestion(String username);
+
+    //校验问题答案是否正确，并生成token
+    ServerResponse<String> checkAnswer(String username, String question, String answer);
+
+    //未登录情况下的忘记密码并修改密码功能
+    ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken);
+
+    //登录后修改密码
+    ServerResponse<String> resetPassword(String passwordOld, String passwordNew, User user);
 }
